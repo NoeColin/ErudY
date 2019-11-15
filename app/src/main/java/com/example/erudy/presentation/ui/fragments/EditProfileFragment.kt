aa21e8818ede44caa4000bf14f7ae689ad88cdbb
NoeColin <noe.colin@yahoo.fr>
2019-11-15 10:33:38 +0100

    save image working

--- app/src/main/java/com/example/erudy/presentation/ui/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/example/erudy/presentation/ui/fragments/EditProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.example.erudy.presentation.ui.fragments
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 
 import com.example.erudy.R
@@ -12,6 +16,9 @@ import com.example.erudy.base.BaseFragment
 import com.example.erudy.presentation.presenter.presenter.EditProfileFragmentPresenter
 import com.example.erudy.presentation.presenter.view.EditProfileView
 import com.example.erudy.presentation.ui.activity.EditProfileActivity
+import com.yanzhenjie.album.Album
+import com.yanzhenjie.durban.Controller
+import com.yanzhenjie.durban.Durban
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
 import kotlinx.android.synthetic.main.fragment_edit_profile.edit_email
@@ -89,6 +96,66 @@ class EditProfileFragment : BaseFragment<EditProfileFragmentPresenter>(), EditPr
 
         save_button.setOnClickListener {
             saveUpdate()
+        }
+
+
+        profile_image.setOnClickListener {
+            Album.image(this) // Image and video mix options.
+                .singleChoice() // Multi-Mode, Single-Mode: singleChoice().
+                .columnCount(3) // The number of columns in the page list.
+                .camera(true) // Whether the camera appears in the Item.
+//                .filterMimeType() // Filter file format.
+                .onResult {
+                    Durban.with(this)
+                        // Che title of the UI.
+                        .title("Crop")
+                        .statusBarColor(ContextCompat.getColor(context!!, R.color.colorErudYPrimaryDark))
+                        .toolBarColor(ContextCompat.getColor(context!!, R.color.colorErudYPrimary))
+                        .navigationBarColor(ContextCompat.getColor(context!!, R.color.colorErudYPrimary))
+                        // Image path list/array.
+                        .inputImagePaths(it[0].path)
+                        // Image output directory.
+                        .outputDirectory(context!!.applicationInfo.dataDir)
+                        // Image size limit.
+                        .maxWidthHeight(500, 500)
+                        // Aspect ratio.
+                        .aspectRatio(1f, 1f)
+                        // Output format: JPEG, PNG.
+                        .compressFormat(Durban.COMPRESS_JPEG)
+                        // Compress quality, see Bitmap#compress(Bitmap.CompressFormat, int, OutputStream)
+                        .compressQuality(90)
+                        // Gesture: ROTATE, SCALE, ALL, NONE.
+                        .gesture(Durban.GESTURE_ALL)
+                        .controller(
+                            Controller.newBuilder() // Create Builder of Controller.
+                                .enable(false) // Enable the control panel.
+                                .rotation(true) // Rotation button.
+                                .rotationTitle(true) // Rotation button title.
+                                .scale(true) // Scale button.
+                                .scaleTitle(true) // Scale button title.
+                                .build()) // Create Controller Config.
+                        .requestCode(200)
+                        .start()
+                }
+                .onCancel {
+
+                }
+                .start()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            200 -> {
+                // Analyze the list of paths after cropping.
+                if (resultCode == Activity.RESULT_OK) {
+                    val mImageList = Durban.parseResult(data!!)
+                    profile_image.setImageURI(mImageList[0].toUri())
+                } else {
+                    Toast.makeText(context, "Error: Try again later", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 
