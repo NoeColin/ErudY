47606a92ff7154f457fbf46717607509438e600f
NoeColin <noe.colin@yahoo.fr>
2019-11-15 15:06:21 +0100

    clear menu + add conv item -> profile

--- app/src/main/java/com/example/erudy/presentation/ui/activity/ContainerActivity.kt
+++ app/src/main/java/com/example/erudy/presentation/ui/activity/ContainerActivity.kt
@@ -45,7 +45,7 @@ class ContainerActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_profile
+                R.id.nav_home, R.id.nav_profile, R.id.nav_conversation
             ), drawerLayout
         )
 
