3dd732f56aca8da3dcc356be416a0f1a78579446
Bigoud <bigoud33@gmail.com>
2019-11-15 17:28:59 +0100

    Add splash

---
+++ app/src/main/java/com/example/erudy/SplashScreenActivity.kt
@@ -0,0 +1,43 @@
+package com.example.erudy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.erudy.presentation.ui.activity.LoginActivity
+
+private const val SPLASH_TIME_OUT: Long = 3000
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_TIME_OUT)
+    }
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+}
+
